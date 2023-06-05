e6e8a123051d49fc9f2c5daeb9017473461b93de
OYELEKE Abdul-Majeed Oyejimi <125889181+PrivateSpace01@users.noreply.github.com>
2023-06-05 13:00:38 +0100

    added the middle-part of the project

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 'use strict'
+
+const more = document.querySelector('.more')
+const imgContainer = document.querySelector('.img-Residence')
+console.log(imgContainer)
+more.addEventListener('click', function () {
+    imgContainer.style.scrollBehavior = 'smooth'
+    imgContainer.scrollLeft += 600
+
+})

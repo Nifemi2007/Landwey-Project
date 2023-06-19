bd076f16eb915d24ad23a5d47722de063573a9c2
QozeemAjadi <qozeemahmed01@gmail.com>
2023-06-19 15:10:30 -0700

    added the footer

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const section = document.querySelector("section");
 const img = document.querySelector(".img1");
 const text = document.querySelector(".text");
 const icon = document.querySelector(".icon");
+const btn = document.querySelector('.btn')
+const button = document.querySelector('.image') 
 
 window.onload = function () {
   section.style.opacity = 1;
@@ -14,10 +16,17 @@ document.addEventListener("scroll", function () {
   icon.style.transform = "scale(1)";
 });
 
+function scroll () {
+  
+    imgContainer.style.scrollBehavior = "smooth";
+    imgContainer.scrollLeft += 600;
+}
+
 const more = document.querySelector(".more");
 const imgContainer = document.querySelector(".img-Residence");
 console.log(imgContainer);
-more.addEventListener("click", function () {
-  imgContainer.style.scrollBehavior = "smooth";
-  imgContainer.scrollLeft += 600;
-});
+more.addEventListener("click", scroll);
+
+btn.addEventListener('click', function() {
+  button.style.scrollLeft += 800
+} )

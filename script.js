e284b1bc489e7a94b85b20f7b3ec1c2ae0b14c26
noyinloy <oyinyinkanifemi@gmail.com>
2026-09-25 11:12:45 +0100

    updated some styling

--- script.js
+++ script.js
@@ -4,19 +4,19 @@ const section = document.querySelector("section");
 const img = document.querySelector(".img1");
 const text = document.querySelector(".text");
 const icon = document.querySelector(".icon");
+const more = document.querySelector(".more");
+const imgContainer = document.querySelector(".img-Residence");
 
 window.onload = function () {
   section.style.opacity = 1;
 };
 
-document.addEventListener("scroll", function () {
-  icon.style.gap = "50px";
-  icon.style.transform = "scale(1)";
-});
+// document.addEventListener("scroll", function () {
+//   icon.style.gap = "50px";
+//   icon.style.transform = "scale(1)";
+// });
 
-const more = document.querySelector(".more");
-const imgContainer = document.querySelector(".img-Residence");
-console.log(imgContainer);
+// console.log(imgContainer);
 more.addEventListener("click", function () {
   imgContainer.style.scrollBehavior = "smooth";
   imgContainer.scrollLeft += 600;
